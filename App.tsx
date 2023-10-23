882adbae00494e69ee1401c14bf4d22a053a3f68
Robbie Luijben <robbie@luijben.tech>
2023-10-23 16:13:18 +0200

    Add webrtc + expo EAS  build

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { mediaDevices, RTCView } from 'react-native-webrtc';
 
 export default function App() {
+  const test = mediaDevices.getUserMedia({ video: true });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
